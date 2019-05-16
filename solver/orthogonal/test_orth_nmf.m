5954dd8fbc39d14a05d7eb7be7868daedb1326fe
Hiroyuki KASAI <kasai@is.uec.ac.jp>
2019-05-16 16:40:16 +0900

    Version 1.6

--- solver/orthogonal/test_orth_nmf.m
+++ solver/orthogonal/test_orth_nmf.m
@@ -5,19 +5,22 @@
 % This file is part of NMFLibrary.
 %
 % Created by H.Kasai on July 23, 2017
+% Modified by H.Kasai on May 16, 2019
 
 clc;
 clear;
 close all;
 
+rng('default')
+
 %% generate synthetic data of (mxn) matrix       
-m = 100;
-n = 100;
+m = 200;
+n = 200;
 V = rand(m,n);
 
 
 %% Initialize of rank to be factorized
-rank = 25;
+rank = 30;
 
 
 %% Initialize of W and H
@@ -25,7 +28,7 @@ options = [];
 [x_init.W, x_init.H] = NNDSVD(abs(V), rank, 0);
 options.x_init = x_init;
 options.verbose = 2;
-options.max_epoch = 300;
+options.max_epoch = 1000;
 
 
 
@@ -47,16 +50,40 @@ options.orth_w    = 1;
 options.norm_w    = 1; 
 [w_orth_w_nmf_mu, infos_orth_w_nmf_mu] = nmf_orth_mu(V, rank, options);
 
+
+% DTPP
+options.norm_h    = 0;
+options.norm_w    = 0;
+
+% DTPP for H
+options.orth_h    = 1;
+options.orth_w    = 0;
+[w_nmf_orth_dtpp_h, infos_nmf_orth_dtpp_h] = nmf_dtpp(V, rank, options);
+
+% DTPP for W
+options.orth_h    = 0;
+options.orth_w    = 1;
+[w_nmf_orth_dtpp_w, infos_nmf_orth_dtpp_w] = nmf_dtpp(V, rank, options);
+
+% DTPP for W & H
+options.orth_h    = 1;
+options.orth_w    = 1;
+[w_nmf_orth_dtpp_wh, infos_nmf_orth_dtpp_wh] = nmf_dtpp(V, rank, options);
+
+
 % NMF-HALS-SO
 options.wo = 1;
-[nmf_hals_so, infos_nmf_hals_so] = nmf_hals_so(V, rank, options);
-
+[w_nmf_hals_so, infos_nmf_hals_so] = nmf_hals_so(V, rank, options);
 
 
 
 %% plot
-display_graph('epoch','cost', {'Orth-MU-H', 'Orth-MU-W', 'NMF-HALS-SO'}, {w_orth_h_nmf_mu, w_orth_w_nmf_mu, nmf_hals_so}, {infos_orth_h_nmf_mu, infos_orth_w_nmf_mu, infos_nmf_hals_so});
-display_graph('epoch','orth', {'Orth-MU-H', 'Orth-MU-W', 'NMF-HALS-SO'}, {w_orth_h_nmf_mu, w_orth_w_nmf_mu, nmf_hals_so}, {infos_orth_h_nmf_mu, infos_orth_w_nmf_mu, infos_nmf_hals_so});
+display_graph('epoch','cost', {'Orth-MU-H', 'Orth-MU-W', 'DTPP-H', 'DTPP-W', 'DTPP-WH', 'NMF-HALS-SO'}, ...
+    {w_orth_h_nmf_mu, w_orth_w_nmf_mu, w_nmf_orth_dtpp_h, w_nmf_orth_dtpp_w, w_nmf_orth_dtpp_wh, w_nmf_hals_so}, ...
+    {infos_orth_h_nmf_mu, infos_orth_w_nmf_mu, infos_nmf_orth_dtpp_h, infos_nmf_orth_dtpp_w, infos_nmf_orth_dtpp_wh, infos_nmf_hals_so});
+display_graph('epoch','orth', {'Orth-MU-H', 'Orth-MU-W', 'DTPP-H', 'DTPP-W', 'DTPP-WH', 'NMF-HALS-SO'}, ...
+    {w_orth_h_nmf_mu, w_orth_w_nmf_mu, w_nmf_orth_dtpp_h, w_nmf_orth_dtpp_w, w_nmf_orth_dtpp_wh, w_nmf_hals_so}, ...
+    {infos_orth_h_nmf_mu, infos_orth_w_nmf_mu, infos_nmf_orth_dtpp_h, infos_nmf_orth_dtpp_w, infos_nmf_orth_dtpp_wh, infos_nmf_hals_so});
 
 
 
