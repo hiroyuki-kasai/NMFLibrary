2fc7143c37f0f26782031f00e54288da92ed5e88
Hiroyuki KASAI <hiroyuki-kasai@users.noreply.github.com>
2022-07-15 09:18:53 +0900

    Version 1.9

--- nmflibrary_version.m
+++ nmflibrary_version.m
@@ -1,5 +1,5 @@
 function [version, release_date] = nmflibrary_version()
-    version         = '1.8.1';
-    release_date    = '14-Oct.-2020';
+    version         = '1.9';
+    release_date    = '15-Jul.-2020';
 end
 
